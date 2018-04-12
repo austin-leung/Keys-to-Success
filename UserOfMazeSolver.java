890a29ad48d0f111387b9d655e5acb66a535b41c
lbushra <34113646+lbushra@users.noreply.github.com>
2018-04-12 00:17:43 -0400

    deleted test requiring ui
    since directions say to test the smallest thing and that would be the base case. unsure if this was the right move though

--- UserOfMazeSolver.java
+++ UserOfMazeSolver.java
@@ -7,9 +7,8 @@
   o  window height?
 
   For example,
-      java UserOfMaze mazes/4cell_treasureWest.txt -1 -1  ??? # no explorer
+      java UserOfMaze mazes/4cell_treasureWest.txt -1 -1 # no explorer
 
-      NOTE: I'm not sure how to express window height
  */
 
       public class UserOfMazeSolver {
@@ -19,15 +18,6 @@
 
       		throws java.io.FileNotFoundException {
       			System.out.println();
-
-      			Maze maze = new Maze( commandLine[0]
-      				, Integer.parseInt( commandLine[1])
-      				, Integer.parseInt( commandLine[2])
-      				);
-
-      			MazeSolver mazeSolver = new MazeSolver (maze, Integer.parseInt( commandLine[3]) );
-//probably need incremental tests in between
-      			System.out.println(mazeSolver); //should print true or false
           
 // testing the base case
             Maze baseCaseMaze = new Maze("/mazes/basecase_wall.txt", 0, 0);
