6faac9b45e65f989e1eaeaa10d128f76ef485240
lbushra <34113646+lbushra@users.noreply.github.com>
2018-04-15 21:41:13 -0400

    displayer...?

--- UserOfMazeSolver.java
+++ UserOfMazeSolver.java
@@ -11,36 +11,55 @@
 
  */
 
-      public class UserOfMazeSolver {
+    public class UserOfMazeSolver {
 		
+	private static Displayer displayer;
 
       	public static void main(String[] commandLine) 
 
-
+	
+	      
       		throws java.io.FileNotFoundException {
 		
-		//		Maze maze = new Maze( commandLine[0]
-		//		, Integer.parseInt( commandLine[1])
-		//		, Integer.parseInt( commandLine[2])
-		//		);
-		//		System.out.println( maze + System.lineSeparator());
-		 
+				Maze maze = new Maze( commandLine[0]
+				, Integer.parseInt( commandLine[1])
+				, Integer.parseInt( commandLine[2])
+				);
+				System.out.println( maze + System.lineSeparator());
+					
+				/**
+				Display changes to a maze.
+				Run by using the height of your shell window as a final argument, like...
+				java UserOfMaze mazes/4cell_treasureWest.txt 0 3 25
+				*/
+				
+				private static void displayerTest( Maze m) {
+					int step = 0;
+
+					displayer.atTopOfWindow( m + "step " + step++);
+
+					// move past west edge, Displaying as we go
+					while( step < 5) {
+						m.go( Maze.WEST);
+						displayer.atTopOfWindow( m + "step " + step++);
+					}
+				}	
 		 
 		 
 		// testing the base cases
 		//expecting false
-				Maze baseCaseMaze = new Maze("mazes/basecase_wall.txt", 0, 0);
-				MazeSolver baseCaseTest = new MazeSolver(baseCaseMaze);
-				System.out.println(baseCaseTest);
+		//		Maze baseCaseMaze = new Maze("mazes/basecase_wall.txt", 0, 0);
+		//		MazeSolver baseCaseTest = new MazeSolver(baseCaseMaze);
+		//		System.out.println(baseCaseTest);
       		
-				System.out.println();
+		//		System.out.println();
 	
 		//expecting true
-				Maze baseCaseTreasure = new Maze("mazes/basecase_treasure.txt", 0, 0);
-				MazeSolver baseCaseTest1 = new MazeSolver(baseCaseTreasure);
-				System.out.println(baseCaseTest1);
+		//		Maze baseCaseTreasure = new Maze("mazes/basecase_treasure.txt", 0, 0);
+		//		MazeSolver baseCaseTest1 = new MazeSolver(baseCaseTreasure);
+		//		System.out.println(baseCaseTest1);
       		
-				System.out.println();
+		//		System.out.println();
 				
 				
 				
@@ -48,18 +67,18 @@
 		//some cases taken from piazza @446
 		
 		//expecting true
-				Maze recursivePathTurn = new Maze("mazes/pathWithTurn.txt", 0, 2);
-				MazeSolver recursiveTest = new MazeSolver(recursivePathTurn);
-				System.out.println(recursiveTest);
+		//		Maze recursivePathTurn = new Maze("mazes/pathWithTurn.txt", 0, 2);
+		//		MazeSolver recursiveTest = new MazeSolver(recursivePathTurn);
+		//		System.out.println(recursiveTest);
       		
-				System.out.println();
+		//		System.out.println();
 		
 		//expecting false
-				Maze recursiveWallHit = new Maze("mazes/wallHit.txt", 0, 0);
-				MazeSolver recursiveTest1 = new MazeSolver(recursiveWallHit);
-				System.out.println(recursiveTest1);
+		//		Maze recursiveWallHit = new Maze("mazes/wallHit.txt", 0, 0);
+		//		MazeSolver recursiveTest1 = new MazeSolver(recursiveWallHit);
+		//		System.out.println(recursiveTest1);
       		
-				System.out.println();
+		//		System.out.println();
 				
 			}
 	  }
