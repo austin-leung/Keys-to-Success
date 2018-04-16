830db3c6fbb80269dcc73703bb35bfe0a4307c1d
lbushra <34113646+lbushra@users.noreply.github.com>
2018-04-15 23:20:32 -0400

    displayer

--- MazeSolver.java
+++ MazeSolver.java
@@ -9,19 +9,28 @@ public class MazeSolver {
     //fields
     // new instances of Maze and Displayer
 	public Maze newMaze;
-	public Displayer displayer;
-	private int stepsTaken;
+	public Displayer newDisplayer;
 
 	public boolean pathExists;
 	private static int[] directionArray = new int[] {Maze.NORTH, Maze.EAST, Maze.SOUTH, Maze.WEST};
 
     //methods
-
-	// base case testing overloading constructor without displayer
-	public MazeSolver (Maze mazeGiven, int heightOfDisplayer) {
+    public MazeSolver (Maze mazeGiven, int windowHeight) {
 
 		newMaze = mazeGiven;
-		displayer = new Displayer( heightOfDisplayer);
+
+        //specify the window height and use Displayer to print out the maze. 
+		newDisplayer = new Displayer(windowHeight);
+		newDisplayer.atTopOfWindow("Maze visualization:\n" + newMaze); //displays window if maze is printed
+
+		//helper method
+		pathExists = mazeSolution();
+	}
+
+	// base case testing overloading constructor without displayer
+	public MazeSolver (Maze mazeGiven) {
+
+		newMaze = mazeGiven;
 
 		//helper method
 		pathExists = mazeSolution();
@@ -39,48 +48,48 @@ public class MazeSolver {
 	}
 
 	private boolean mazeSolution() {
-		
-		displayer.atTopOfWindow( newMaze + "step " + stepsTaken++);
-		
+
+		//pseudocode here
+
 		//base cases
 		if (newMaze.explorerIsOnA() == Maze.TREASURE) {
-
+		
 			// test if the base case is working for just treasure
 			//return false;
 
 			return true;
 		}
-		
 		else if (newMaze.explorerIsOnA() == Maze.WALL) {
-			
 			// test if the base case is working for just a wall
-            // return true; 
-			
+           		 // return true; 
 			return false;
 		}
 
 		//recursive cases
 		else {
-			
+
+			Maze snap = new Maze(newMaze); //snapshot of the maze so you can go back if this direction doesn't work out
+
 			for (int direction:directionArray) {
 
-				Maze snap = new Maze(newMaze); //snapshot of the maze so you can go back if this direction doesn't work out
+				
 				newMaze.dropA(Maze.WALL); //place a wall so you don't go back
 				newMaze.go(direction); //step forward
+
+				System.out.print ("After moving:\n" + newMaze);
 				
 
-			//recursive abstraction
-				if (mazeSolution()) { //if there is a valid solution
-					return mazeSolution(); //aka true
-				}
+            //recursive abstraction
+            if (mazeSolution()) { 		//if there is a valid solution
+	          return mazeSolution();    	//aka true
+	      }
 
-			newMaze = snap; //step back if it didn't work
+		newMaze = new Maze(snap); 	//step back if it didn't work
 
-			}
-	
-			return false; //should only be reached if you've exhausted all paths
-	
-		}
-	}
+
+	  }
+                return false; //should only be reached if you've exhausted all paths
+            }
+        }
 
 }
